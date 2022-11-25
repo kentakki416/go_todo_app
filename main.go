c39e641d43c76415e0e03bc6fd406e084ec20fc7
kentakki416 <fujiken0814@outlook.jp>
2022-11-25 23:37:15 +0900

    mainをリファクタ

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/kentakki416/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -23,8 +18,6 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -35,32 +28,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	// *http.Server型を使うとサーバーのタイムアウト時間など柔軟に設定できる
-	s := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
-	// errgruup.Group型を使うと戻り値にエラーが含まれるゴルーチン間の並行処理の実装が簡単になる
-	eg, ctx := errgroup.WithContext(ctx)
-
-	// 別ゴルーチンでHTTPサーバーを起動
-	eg.Go(func() error {
-		// http.ErrServerClosedはhttp.Server.Shatdown()が正常に終了したことを示す
-		if err := s.Serve(l); err != nil && err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// チャネルからの通知（終了通知）を待機する
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdwn: %+v", err)
-	}
-
-	// Goメソッドで起動した別のゴルーチンの終了を待つ
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
